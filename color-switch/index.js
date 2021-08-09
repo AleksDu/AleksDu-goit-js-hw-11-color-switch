a3db95a10254cab447d5cb3481bb1184f59f1e63
AleksDu <aleksandr.dyshkin@gmail.com>
2021-08-09 20:40:05 +0300

    finish color-switc

---
+++ color-switch/index.js
@@ -0,0 +1,38 @@
+import { colors } from './src/colors.js'
+
+const refs = {
+    start: document.querySelector('button[data-action="start"]'),
+    stop: document.querySelector('button[data-action="stop"]'),
+    colorBody: document.body,
+};
+
+refs.start.addEventListener('click', onChangeColor);
+
+const TIME = 1000;
+
+function onChangeColor() {
+
+    const intervalID = setInterval(() => {
+        getRandomColor()
+    }, TIME);
+
+    if (intervalID) {
+        refs.start.setAttribute('disabled', true);
+    };
+
+    refs.stop.addEventListener('click',
+        () => {
+            clearInterval(intervalID);
+            refs.start.removeAttribute('disabled');
+        });
+};
+
+function getRandomColor() {
+    let num = randomIntegerFromInterval(0, colors.length - 1);
+
+    refs.colorBody.setAttribute('style', `background-color:${colors[num]}`);
+};
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
